eb0f7e08610ddae33c83d08c9500847b13a5cc30
alfredodera <alfie.ogada@gmail.com>
2022-04-05 13:04:12 +0300

    simple changes

--- sendmail.php
+++ sendmail.php
@@ -11,14 +11,14 @@ if (mail($to_email, $subject, $body, $headers)) {
     echo "Email sending failed...";
 }
 
-<?php
-$to_email = "receipient@gmail.com";
+$to_email = "alfie.ogada@gmail.com";
 $subject = "Simple Email Test via PHP";
 $body = "Hi,nn This is test email send by PHP Script";
 $headers = "From: sender\'s email";
- 
+
 if (mail($to_email, $subject, $body, $headers)) {
     echo "Email successfully sent to $to_email...";
 } else {
     echo "Email sending failed...";
 }
+?>
